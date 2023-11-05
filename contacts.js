7a5f462bc06e49abec4a07cc6d1151a20a07d254
vladkasyan <voadkasyan.2006@gmail.com>
2023-11-05 13:49:14 +0200

    start

---
+++ contacts.js
@@ -0,0 +1,6 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+console.log(contactsPath);
